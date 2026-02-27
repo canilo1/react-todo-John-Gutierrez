86defa02aea4d627e289ee9ee6679fbe2dd36864
John Gutierrez <75353216+canilo1@users.noreply.github.com>
2026-02-27 14:37:18 -0800

    Update InputWithLabel.jsx

--- src/components/InputWithLabel.jsx
+++ src/components/InputWithLabel.jsx
@@ -1,32 +1,34 @@
 import React, { useRef, useEffect } from "react";
-import PropTypes from 'prop-types'; // Import prop-types
+import PropTypes from "prop-types";
 
-function InputWithLabel(props) {
-    const inputRef = useRef(null);
+function InputWithLabel({ id, name, todoTitle, handleTitleChange, children }) {
+  const inputRef = useRef(null);
 
-    useEffect(() => {
-        inputRef.current.focus();
-    }, []);
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
-    return (
-        <>
-            <label htmlFor="todoTitle">{props.children}</label>
-            <input
-                id="todoTitle"
-                name="title"
-                value={props.todoTitle}
-                onChange={props.handleTitleChange}
-                ref={inputRef}
-            />
-        </>
-    );
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      <input
+        id={id}
+        name={name}
+        type="text"
+        value={todoTitle}
+        onChange={handleTitleChange}
+        ref={inputRef}
+      />
+    </>
+  );
 }
 
-// Define propTypes
 InputWithLabel.propTypes = {
-    children: PropTypes.node.isRequired, 
-    todoTitle: PropTypes.string.isRequired, 
-    handleTitleChange: PropTypes.func.isRequired, // The handleTitleChange prop should be a function
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  todoTitle: PropTypes.string.isRequired,
+  handleTitleChange: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default InputWithLabel;
